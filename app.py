8c57031e64f53ac378a6c5585ba1aa5072262396
root <midnightlunalux@gmail.com>
2023-05-30 22:24:07 -0400

    committing files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+from sqlalchemy.sql import text
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secretsecret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/')
+def home_page():
+    """Shows homepage"""
+    print(db)
+    return render_template('index.html')
+
+db.create_all()
